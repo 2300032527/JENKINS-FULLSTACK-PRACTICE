09119bb0930d0fec5b10b0dbd7f6edc818147f83
SSL <2300032527@kluniversity.in>
2025-09-23 14:33:08 +0530

    updated

--- Lfrontend/Library/src/config.js
+++ Lfrontend/Library/src/config.js
@@ -1,6 +1,6 @@
 // Backend API configuration
 const config = {
-  backendUrl: "http://localhost:2030/springbootlibraryapi" // Change this to your backend URL
+  backendUrl: "/springbootlibraryapi" // Change this to your backend URL
 };
 
 export default config;
