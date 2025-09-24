d089df1e35624002334766025af443676601fc06
SSL <2300032527@kluniversity.in>
2025-09-24 13:38:25 +0530

    Final backend updation

--- Lbackend/Library/src/main/java/com/example/library/controller/BookController.java
+++ Lbackend/Library/src/main/java/com/example/library/controller/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-@CrossOrigin(origins = "http://localhost:5173") 
+@CrossOrigin(origins = "*") 
 public class BookController {
     private final BookService service;
     public BookController(BookService service) { this.service = service; }
